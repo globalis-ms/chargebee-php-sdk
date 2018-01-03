24d9a612f020c4f21e1eb7bef23e8911a8ae72fa
Nathan Dunn <me@ndunn.uk>
2018-01-03 23:53:27 +0000

    Add Address resource

---
+++ src/Api/Addresses/Address.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace NathanDunn\Chargebee\Api\Addresses;
+
+use Http\Client\Exception;
+use NathanDunn\Chargebee\Api\AbstractApi;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class Address extends AbstractApi
+{
+    /**
+     * @param array $parameters
+     * @return array|string
+     * @throws Exception
+     */
+    public function find(array $parameters)
+    {
+        $resolver = new OptionsResolver;
+
+        $resolver->setDefined('subscription_id')
+            ->setAllowedTypes('subscription_id', 'string')
+            ->setRequired('subscription_id');
+
+        $resolver->setDefined('label')
+            ->setAllowedTypes('label', 'string')
+            ->setRequired('label');
+
+        $url = $this->url('addresses');
+
+        return $this->get($url, $resolver->resolve($parameters));
+    }
+
+    /**
+     * @param array $data
+     * @return array|string
+     * @throws Exception
+     */
+    public function update(array $data)
+    {
+        $url = $this->url('addresses');
+
+        return $this->post($url, $data);
+    }
+}
---
+++ tests/Unit/Api/Addresses/AddressTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Tests\Unit\Api\Addresses;
+
+use NathanDunn\Chargebee\Api\Addresses\Address;
+use Tests\Unit\Api\TestCase;
+
+class AddressTest extends TestCase
+{
+    /** @test */
+    public function should_find_address()
+    {
+        $expected = $this->getContent(sprintf('%s/data/responses/address.json', __DIR__));
+
+        $address = $this->getApiMock();
+        $address->expects($this->once())
+            ->method('get')
+            ->with('https://123456789.chargebee.com/api/v2/addresses')
+            ->will($this->returnValue($expected));
+
+        $this->assertEquals($expected, $address->find(['subscription_id' => '8avVGOkx8U1MX', 'label' => 'shipping_address']));
+    }
+
+    /** @test */
+    public function should_update_address()
+    {
+        $expected = $this->getContent(sprintf('%s/data/responses/address_updated.json', __DIR__));
+
+        $address = $this->getApiMock();
+        $address->expects($this->once())
+            ->method('post')
+            ->with('https://123456789.chargebee.com/api/v2/addresses')
+            ->will($this->returnValue($expected));
+
+        $this->assertEquals($expected, $address->update([]));
+    }
+
+    /**
+     * @return string
+     */
+    protected function getApiClass()
+    {
+        return Address::class;
+    }
+}
