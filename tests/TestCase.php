e2c6b0616c71a7041c30b2289ae12c0c47f71482
Nathan Dunn <me@ndunn.uk>
2018-01-03 23:45:05 +0000

    Add base TestCase

---
+++ tests/TestCase.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests;
+
+use Http\Discovery\MessageFactoryDiscovery;
+use Http\Discovery\StreamFactoryDiscovery;
+use Http\Mock\Client as MockClient;
+use NathanDunn\Chargebee\Client;
+use NathanDunn\Chargebee\HttpClient\Builder;
+use PHPUnit\Framework\TestCase as PHPUnitTestCase;
+
+class TestCase extends PHPUnitTestCase
+{
+    /**
+     * @var string
+     */
+    public static $key = '987654321';
+
+    /**
+     * @var string
+     */
+    public static $site = '123456789';
+
+    /**
+     * @var Builder
+     */
+    public $builder;
+
+    /**
+     * @var Client
+     */
+    public $client;
+
+    /**
+     * Set up test case
+     */
+    public function setUp()
+    {
+        $this->builder = new Builder(
+            self::$key, new MockClient, MessageFactoryDiscovery::find(), StreamFactoryDiscovery::find()
+        );
+        $this->client = new Client(self::$site, self::$key, $this->builder);
+    }
+}
